ae3c99bfc55cd6437ea0ab9736aded855cfffdd0
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-07-02 11:01:10 -0400

    in app.js addTodo function, change newTodo innerText to todoInput.value

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const addTodo = (event) => {
   todoDiv.classList.add('todo');
 
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'hey';
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
 
